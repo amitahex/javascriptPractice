5f30b4c71995af364dbb8134b6e2e6ddd06fd1fb
amitahex <amit.kumar@ahex.co.in>
2023-05-25 23:04:20 +0530

    done 2 question

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1.Implement a function that takes an array of numbers as input and returns the sum of all the numbers.
+
+// let numberArray=[1,2,3,4,5]
+// let add=0
+// function sum(number){
+//     for(let i=0;i<number.length;i++){
+//      add = number[i] + add
+//     }
+//     console.log(add);
+
+// }
+// sum(numberArray);
+/////********************************************************* */////////////
+
+// 2.Write a function that takes an array of strings as input and returns a new array with all the strings capitalized.
+
+// let letters=['a', 'b', 'c', 'd', 'e'];
+// let capitalizeArray=[];
+
+// function capitalizeLetters(letter){
+//     for(let i=0;i<letter.length;i++){
+//     capitalizeArray.push(letter[i].toUpperCase());
+//     }
+//     console.log(capitalizeArray);
+// }
+// capitalizeLetters(letters)
+
+////************************************************************************************** */
+
+// 3.Implement a function that takes a string as input and returns the number of vowels in the string.
